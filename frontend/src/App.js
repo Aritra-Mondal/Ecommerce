8f190574b656b242f96e6c6a80f80a12487e9b3f
Aritra-Mondal <aritra.mondal.0538@gmail.com>
2021-10-18 17:20:27 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import ProductPage from "./Pages/ProductPage";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/product/:id">
+            <ProductPage />
+          </Route>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;

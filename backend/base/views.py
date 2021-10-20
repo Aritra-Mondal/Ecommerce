069094d3c8851d3c869360bab67ca708594f739e
Aritra-Mondal <aritra.mondal.0538@gmail.com>
2021-10-20 12:01:27 +0530

    checking rest-framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .products import products
 
 # Create your views here.
@@ -7,5 +9,16 @@ from .products import products
 def getRoutes(request):
       return JsonResponse('Hello', safe=False)
 
+
+@api_view(["GET"])
 def getProducts(request):
-      return JsonResponse(products, safe=False)
+      return Response(products)
+
+@api_view(["GET"])
+def getProduct(request,pk):
+      product = None
+      for i in products:
+            if i['_id'] == pk :
+                  product = i
+                  break
+      return Response(product)
